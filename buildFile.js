2dd4dc5d5e533eec5d74197777b6ef7eb0885988
Poomathi <poomathi.k@kaaviansys.com>
2022-11-29 12:49:30 +0530

    Add Module for builtStaticFiles..

--- buildFile.js
+++ buildFile.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 // for any other request, serve HTML in DIT environment (cloud env)
-exports.build = function(NODE_ENV, fileLocation){
+exports.build = function(NODE_ENV, directoryName, filePath){
   if (NODE_ENV === 'DIT') {
     const indexHTMLContent = fs.readFileSync(
-      path.join(fileLocation),
+      path.join(directoryName + filePath),
       'utf8'
     );
     app.all('*', (req, res) => {
       res.send(indexHTMLContent);
     });
   }
+}
+
+// for serving built static js/css files
+exports.builtStaticFiles = function(folderName, directoryName, filePath){
+  app.use(
+    folderName,
+    express.static(path.join(directoryName, filePath))
+  );
 }
